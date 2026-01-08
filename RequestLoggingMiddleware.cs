3140a153f6077825fa72d0f5b790ac333793f464
routashish2457-oss <routashish2457@gmail.com>
2026-01-08 20:51:46 +0530

    Add RequestLoggingMiddleware for logging requests

---
+++ RequestLoggingMiddleware.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+
+namespace DotNetApiProject.Middleware
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
+            await _next(context);
+        }
+    }
+
+    public static class RequestLoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestLoggingMiddleware>();
+        }
+    }
+}
+
